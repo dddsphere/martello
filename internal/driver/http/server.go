2c6f0cdede42f42a927b288b1fce339e1679b1e8
Adrian K <me@adrianpk.com>
2023-05-17 18:22:13 +0200

    HTTP server bare implementation

--- internal/driver/http/server.go
+++ internal/driver/http/server.go
@@ -1,7 +1,12 @@
-package rest
+package http
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/dddsphere/martello/internal/config"
 	"github.com/dddsphere/martello/internal/log"
@@ -11,6 +16,7 @@ import (
 type (
 	Server struct {
 		system.Worker
+		http.Server
 		router http.Handler
 	}
 )
@@ -23,4 +29,55 @@ func NewServer(name string, cfg *config.Config, log log.Logger) (server *Server)
 
 func (srv *Server) SetRouter(router http.Handler) {
 	srv.router = router
+}
+
+func (srv *Server) StartTask(ctx context.Context) system.Task {
+	return srv.Start
+}
+
+func (srv *Server) Start(ctx context.Context) error {
+	srv.Server = http.Server{
+		Addr:    srv.Address(),
+		Handler: srv.router,
+	}
+
+	group, errGrpCtx := errgroup.WithContext(ctx)
+	group.Go(func() error {
+		srv.Log().Infof("%s started listening at %s", srv.Name(), srv.Address())
+		defer srv.Log().Errorf("%s shutdown", srv.Name())
+
+		err := srv.Server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			return err
+		}
+
+		return nil
+	})
+
+	group.Go(func() error {
+		<-errGrpCtx.Done()
+		srv.Log().Errorf("%s shutdown", srv.Name())
+
+		ctx, cancel := context.WithTimeout(context.Background(), srv.ShutdownTimeout())
+		defer cancel()
+
+		if err := srv.Server.Shutdown(ctx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return group.Wait()
+}
+
+func (srv *Server) Address() string {
+	host := srv.Cfg().ValOrDef("http.server.host", "0.0.0.0")
+	port := srv.Cfg().ValAsInt("http.server.port", 8080)
+	return fmt.Sprintf("%s:%d", host, port)
+}
+
+func (srv *Server) ShutdownTimeout() time.Duration {
+	secs := time.Duration(srv.Cfg().ValAsInt("http.server.shutdown.timeout.secs", 12))
+	return secs * time.Second
 }
