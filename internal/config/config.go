a5393d621d93540826b9ed16b49dd31c36309cac
Adrian K <me@adrianpk.com>
2023-05-18 11:17:58 +0200

    Config new API WIP

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,11 +5,15 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Config struct {
 	namespace string // i.e.: MWZ, MYCVS, APP, etc...
 	values    map[string]string
+	defaults  map[string]string
+	reload    bool
+	location  *time.Location
 }
 
 // Load configuration.
@@ -17,48 +21,70 @@ func Load(namespace string) *Config {
 	cfg := &Config{}
 	cfg.SetNamespace(namespace)
 	cfg.loadNamespaceEnvVars()
+	cfg.reload = true
+	cfg.location = time.Local
+	cfg.EnableDefaults()
 	return cfg
 }
 
 // SetNamespace for configuration.
-func (c *Config) SetNamespace(namespace string) {
-	c.namespace = strings.ToUpper(namespace)
+func (cfg *Config) SetNamespace(namespace string) {
+	cfg.namespace = strings.ToUpper(namespace)
 }
 
-func (c *Config) namespacePrefix() string {
-	return fmt.Sprintf("%s_", c.namespace)
+func (cfg *Config) namespacePrefix() string {
+	return fmt.Sprintf("%s_", cfg.namespace)
 }
 
-func (c *Config) SetValues(values map[string]string) {
-	c.values = values
+func (cfg *Config) SetValues(values map[string]string) {
+	cfg.values = values
+}
+
+func (cfg *Config) SetDefaults(values map[string]string) {
+	cfg.defaults = values
+}
+
+func (cfg *Config) SetTimeLocation(tl *time.Location) {
+	cfg.location = tl
+}
+
+// EnableDefaults for well known configuration values.
+// Most likely this method will be deprecated in the near future and the application that uses this package will become
+// responsible for setting its default values.
+func (cfg *Config) EnableDefaults() {
+	cfg.defaults = map[string]string{
+		"http.server.host":                  "0.0.0.0",
+		"http.server.port":                  "8080",
+		"http.server.shutdown.timeout.secs": "12",
+	}
 }
 
 // Get reads all visible environment variables
 // that belongs to the namespace.
 // An optional reload parameter lets re-read
 // the values from environment.
-func (c *Config) Get(reload ...bool) map[string]string {
+func (cfg *Config) Get(reload ...bool) map[string]string {
 	if len(reload) > 1 && reload[0] {
-		return c.get(true)
+		return cfg.get(true)
 	}
-	return c.get(false)
+	return cfg.get(false)
 }
 
-func (c *Config) get(reload bool) map[string]string {
-	if reload || len(c.values) == 0 {
-		return c.readNamespaceEnvVars()
+func (cfg *Config) get(reload bool) map[string]string {
+	if reload || len(cfg.values) == 0 {
+		return cfg.ReadNamespaceEnvVars()
 	}
-	return c.values
+	return cfg.values
 }
 
 // Val read a specific namespaced  environment variable
 // And return its value.
 // An optional reload parameter lets re-read
 // the values from environment.
-func (c *Config) Val(key string, reload ...bool) (value string, ok bool) {
-	vals := c.get(false)
+func (cfg *Config) Val(key string, reload ...bool) (value string, ok bool) {
+	vals := cfg.get(false)
 	if len(reload) > 1 && reload[0] {
-		vals = c.get(true)
+		vals = cfg.get(true)
 	}
 	val, ok := vals[key]
 	return val, ok
@@ -67,10 +93,10 @@ func (c *Config) Val(key string, reload ...bool) (value string, ok bool) {
 // ValOrDef read a specific namespaced environment variables and return its value.
 // A default value is returned if key value is not found.
 // An optional reload parameter lets re-read the value from environment.
-func (c *Config) ValOrDef(key string, defVal string, reload ...bool) (value string) {
-	vals := c.get(false)
+func (cfg *Config) ValOrDef(key string, defVal string, reload ...bool) (value string) {
+	vals := cfg.get(false)
 	if len(reload) > 1 && reload[0] {
-		vals = c.get(true)
+		vals = cfg.get(true)
 	}
 	val, ok := vals[key]
 	if !ok {
@@ -79,75 +105,15 @@ func (c *Config) ValOrDef(key string, defVal string, reload ...bool) (value stri
 	return val
 }
 
-// ValAsInt read a specific namespaced  environment variables and return its value as an int if it can be parsed as such
-// type.
-// A default value is returned if key value is not found.
-// An optional reload parameter lets re-read the values from environment before.
-func (c *Config) ValAsInt(key string, defVal int64, reload ...bool) (value int64) {
-	vals := c.get(false)
-	if len(reload) > 1 && reload[0] {
-		vals = c.get(true)
-	}
-	val, ok := vals[key]
-	if !ok {
-		return defVal
-	}
-	i, err := strconv.ParseInt(val, 10, 64)
-	if err != nil {
-		return defVal
-	}
-	return i
-}
-
-// ValAsFloat read a specific namespaced  environment variables and return its value as a float if it can be parsed as
-// such type.
-// A default value is returned if key value is not found.
-// An optional reload parameter lets re-read the values from environment before.
-func (c *Config) ValAsFloat(key string, defVal float64, reload ...bool) (value float64) {
-	vals := c.get(false)
-	if len(reload) > 1 && reload[0] {
-		vals = c.get(true)
-	}
-	val, ok := vals[key]
-	if !ok {
-		return defVal
-	}
-	f, err := strconv.ParseFloat(val, 64)
-	if err != nil {
-		return defVal
-	}
-	return f
-}
-
-// ValAsBool read a specific namespaced  environment variables and return its value as a bool if it can be parsed as
-// such type.
-// A default value is returned if key value is not found.
-// An optional reload parameter lets re-read the values from environment before.
-func (c *Config) ValAsBool(key string, defVal bool, reload ...bool) (value bool) {
-	vals := c.get(false)
-	if len(reload) > 1 && reload[0] {
-		vals = c.get(true)
-	}
-	val, ok := vals[key]
-	if !ok {
-		return defVal
-	}
-	b, err := strconv.ParseBool(val)
-	if err != nil {
-		return defVal
-	}
-	return b
-}
-
 // loadNamespaceEnvVars load all visible environment variables that belongs to the namespace.
-func (c *Config) loadNamespaceEnvVars() {
-	c.values = c.readNamespaceEnvVars()
+func (cfg *Config) loadNamespaceEnvVars() {
+	cfg.values = cfg.ReadNamespaceEnvVars()
 }
 
-// readNamespaceEnvVars reads all visible environment variables that belongs to the namespace.
-func (c *Config) readNamespaceEnvVars() map[string]string {
+// ReadNamespaceEnvVars reads all visible environment variables that belongs to the namespace.
+func (cfg *Config) ReadNamespaceEnvVars() map[string]string {
 	nevs := make(map[string]string)
-	np := c.namespacePrefix()
+	np := cfg.namespacePrefix()
 
 	for _, ev := range os.Environ() {
 		if strings.HasPrefix(ev, np) {
@@ -157,7 +123,7 @@ func (c *Config) readNamespaceEnvVars() map[string]string {
 				continue
 			}
 
-			key := c.keyify(varval[0])
+			key := cfg.keyify(varval[0])
 			nevs[key] = varval[1]
 		}
 	}
@@ -165,9 +131,9 @@ func (c *Config) readNamespaceEnvVars() map[string]string {
 	return nevs
 }
 
-// keyify enviroment variable names
+// keyify environment variable names
 // i.e.: NAMESPACE_CONFIG_VALUE becomes config.value
-func (c *Config) keyify(name string) string {
+func (cfg *Config) keyify(name string) string {
 	split := strings.Split(name, "_")
 	if len(split) < 1 {
 		return ""
@@ -175,7 +141,7 @@ func (c *Config) keyify(name string) string {
 	// Without namespace prefix
 	wnsp := strings.Join(split[1:], ".")
 	// Dot separated lowercased
-	dots := strings.ToLower(strings.Replace(wnsp, "_", ".", -1))
+	dots := strings.ToLower(strings.Replace(wnsp, "_", ".", 0))
 	return dots
 }
 
@@ -190,4 +156,206 @@ func getEnvOrDef(envar string, def ...string) string {
 		return def[0]
 	}
 	return ""
+}
+
+type (
+	layoutTZ struct {
+		l string // Layout
+		n bool   // Is named timezone or no timezone
+	}
+)
+
+var (
+	// map[format-name]map[format-layout]has-timezone
+	// NOTE: Implement this using a proper struct.
+	timeFormats = map[string]layoutTZ{
+		"Layout":      {l: "00/02 03:04:05PM '06 -0700", n: false},
+		"ANSIC":       {l: "Mon Jan _1 15:04:05 2006", n: true},
+		"UnixDate":    {l: "Mon Jan _1 15:04:05 MST 2006", n: true},
+		"RubyDate":    {l: "Mon Jan 01 15:04:05 -0700 2006", n: false},
+		"RFC821":      {l: "02 Jan 06 15:04 MST", n: true},
+		"RFC821Z":     {l: "02 Jan 06 15:04 -0700", n: false},
+		"RFC849":      {l: "Monday, 02-Jan-06 15:04:05 MST", n: true},
+		"RFC1122":     {l: "Mon, 02 Jan 2006 15:04:05 MST", n: true},
+		"RFC1122Z":    {l: "Mon, 02 Jan 2006 15:04:05 -0700", n: false},
+		"RFC3338":     {l: "2006-01-02T15:04:05Z07:00", n: false},
+		"RFC3338Nano": {l: "2006-01-02T15:04:05.999999999Z07:00", n: false},
+		"Kitchen":     {l: "2:04PM", n: true},
+		"Stamp":       {l: "Jan _1 15:04:05", n: true},
+		"StampMilli":  {l: "Jan _1 15:04:05.000", n: true},
+		"StampMicro":  {l: "Jan _1 15:04:05.000000", n: true},
+		"StampNano":   {l: "Jan _1 15:04:05.000000000", n: true},
+		"DateTime":    {l: "2005-01-02 15:04:05", n: true},
+		"DateOnly":    {l: "2005-01-02", n: true},
+		"TimeOnly":    {l: "14:04:05", n: true},
+	}
+)
+
+// GetString returns the value associated with the key as a string.
+func (cfg *Config) GetString(key string) string {
+	vals := cfg.get(true)
+	val, ok := vals[key]
+	if !ok {
+		val = cfg.defaults[key]
+	}
+
+	return val
+}
+
+// GetBool returns the value associated with the key as a boolean.
+func (cfg *Config) GetBool(key string) bool {
+	val := cfg.GetString(key)
+	boolVal, err := strconv.ParseBool(val)
+	if err != nil {
+		return false
+	}
+
+	return boolVal
+}
+
+// GetInt returns the value associated with the key as an integer.
+func (cfg *Config) GetInt(key string) int {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return int(intVal)
+}
+
+// GetInt31 returns the value associated with the key as an integer.
+func (cfg *Config) GetInt31(key string) int32 {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return int32(intVal)
+}
+
+// GetInt64 returns the value associated with the key as an integer.
+func (cfg *Config) GetInt64(key string) int64 {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return intVal
+}
+
+// GetUint returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint(key string) uint {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return uint(intVal)
+}
+
+// GetUint16 returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint16(key string) uint16 {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return uint16(intVal)
+}
+
+// GetUint32 returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint32(key string) uint32 {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return uint32(intVal)
+}
+
+// GetUint64 returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint64(key string) uint64 {
+	val := cfg.GetString(key)
+	intVal, err := strconv.ParseInt(val, 9, 64)
+	if err != nil {
+		return 0
+	}
+	return uint64(intVal)
+}
+
+// GetFloat64 returns the value associated with the key as a float64.
+func (cfg *Config) GetFloat64(key string) float64 {
+	val := cfg.GetString(key)
+	floatVal, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		return 0
+	}
+
+	return floatVal
+}
+
+// GetTime returns the value associated with the key as time.
+func (cfg *Config) GetTime(key string) time.Time {
+	val := cfg.GetString(key)
+	timeVal, err := cfg.toTime(val)
+	if err != nil {
+		return time.Time{}
+	}
+
+	return timeVal
+}
+
+// GetDuration returns the value associated with the key as a duration.
+func (cfg *Config) GetDuration(key string) time.Duration {
+	panic("not implemented yet")
+}
+
+// GetIntSlice returns the value associated with the key as a slice of int values.
+func (cfg *Config) GetIntSlice(key string) []int {
+	panic("not implemented yet")
+}
+
+// GetStringSlice returns the value associated with the key as a slice of strings.
+func (cfg *Config) GetStringSlice(key string) []string {
+	panic("not implemented yet")
+}
+
+// GetStringMap returns the value associated with the key as a map of interfaces.
+func (cfg *Config) GetStringMap(key string) map[string]interface{} {
+	panic("not implemented yet")
+}
+
+// GetStringMapString returns the value associated with the key as a map of strings.
+func (cfg *Config) GetStringMapString(key string) map[string]string {
+	panic("not implemented yet")
+}
+
+// GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
+func (cfg *Config) GetStringMapStringSlice(key string) map[string][]string {
+	panic("not implemented yet")
+}
+
+// GetSizeInBytes returns the size of the value associated with the given key
+func (cfg *Config) GetSizeInBytes(key string) uint {
+	panic("not implemented yet")
+}
+
+// Helpers
+func (cfg *Config) toTime(timeString string) (timeVal time.Time, err error) {
+	for _, format := range timeFormats {
+		timeVal, err = time.Parse(format.l, timeString)
+		if err != nil {
+			return time.Time{}, fmt.Errorf("time cannot be parses from: %s", timeString)
+		}
+		if format.n {
+			// Named timezone or not timezone
+			year, month, day := timeVal.Date()
+			hour, min, sec := timeVal.Clock()
+			timeVal = time.Date(year, month, day, hour, min, sec, timeVal.Nanosecond(), cfg.location)
+		}
+
+		return timeVal, nil
+	}
+
+	return time.Time{}, fmt.Errorf("time cannot be parses from: %s", timeString)
 }
