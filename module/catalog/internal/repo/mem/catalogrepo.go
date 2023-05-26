ff4eca10272f07ebe39aaa5793f4bb94fac4253e
Adrian K <me@adrianpk.com>
2023-05-26 18:27:08 +0200

    Catalog bare implementation

---
+++ module/catalog/internal/repo/mem/catalogrepo.go
@@ -0,0 +1,19 @@
+package mem
+
+import "context"
+
+type (
+	CatalogRepo struct {
+		// TODO: WIP
+	}
+)
+
+func (cr *CatalogRepo) Get(ctx context.Context, id string) error {
+	// TODO: WIP
+	return nil
+}
+
+func (cr *CatalogRepo) Save(cid string) error {
+	// TODO: WIP
+	return nil
+}
